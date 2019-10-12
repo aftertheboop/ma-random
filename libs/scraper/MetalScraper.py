07967b7a0eac5760ec38824e979c2b462608367f
Rory Molyneux <rorymolyneux@gmail.com>
2019-10-12 15:33:12 +0200

    Scraper works!

--- libs/scraper/MetalScraper.py
+++ libs/scraper/MetalScraper.py
@@ -1,5 +1,7 @@
 import sqlite3
 import time
+import re
+from .ScraperHelper import ScraperHelper
 
 DB = 'db/bands.sqlite3'
 CONN = sqlite3.connect(DB)
@@ -8,8 +10,8 @@ class MetalScraper:
   def __init__(self):    
     self.displaystart = self.getdisplaystart()
     self.displaylength = 200
-    self.baseurl = "../../mock/api.json"
-    # self.baseurl = "https://www.metal-archives.com/search/ajax-advanced/searching/bands/?iDisplayStart={self.displaystart}&iDisplayLength=200
+    #self.baseurl = "http://localhost/ma-test/search.json"
+    self.baseurl = "https://www.metal-archives.com/search/ajax-advanced/searching/bands/"
 
   def scrape(self):
     queryParams = {
@@ -18,12 +20,53 @@ class MetalScraper:
     }
     target = self.baseurl + '?' + "&".join(queryParams.values())
     # do scrape
-    self.displaystart = int(self.displaystart) + int(self.displaylength)
-    self.updatedisplaystart(self.displaystart)
-    print(target)
-    time.sleep(2)
-    self.scrape()
+    response = self.consume(target)
 
+    if response['aaData']:
+      self.process(response['aaData'])
+      print(target)
+      # scrape complete
+      self.displaystart = int(self.displaystart) + int(self.displaylength)
+      self.updatedisplaystart(self.displaystart)
+      # naptime
+      time.sleep(2)
+      self.scrape()
+
+  def process(self, data):
+    for item in data:
+      match = re.search(r'<a href=".*/(\d+)">(.*)<\/a>.*', item[0])
+      name = match.group(2)
+      ma_id = match.group(1)
+      self.savetodb(ma_id, name)
+
+  def savetodb(self, id, name):
+    bandid = self.getbandbymaid(id)
+    if not bandid:
+      self.insertband(id, name)
+
+  def insertband(self, id, name):
+    cur = CONN.cursor()
+    sql = "INSERT INTO maBands (ma_id, name) VALUES (?, ?);"
+    values = [id, name]
+    cur.execute(sql, values)
+    CONN.commit()
+    
+  def getbandbymaid(self, maid):
+    cur = CONN.cursor()
+    sql = "SELECT id FROM maBands WHERE ma_id = ? LIMIT 1;"
+    values = [maid]
+    cur.execute(sql, values)
+    row = cur.fetchone()
+    if not row:
+      return False
+    else:
+      return row[0]
+
+  def consume(self, target):
+    helper = ScraperHelper()
+    rawdata = helper.get(target)
+    parseddata = helper.parse(rawdata)
+    return parseddata
 
   def getdisplaystart(self):
     cur = CONN.cursor()
