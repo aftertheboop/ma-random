b2249268de00427bc0c12de795bed7ab01510e67
Rory Molyneux <rorymolyneux@gmail.com>
2019-10-12 18:28:15 +0200

    localhost API now works to dynamically pick a band and if no content exists, it scrapes the MA page and adds to the DB

--- index.py
+++ index.py
@@ -1,39 +1,69 @@
-import random
-import urllib.request
-import sqlite3
+import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from libs.RandomBand import RandomBand
-from libs.scraper.MetalScraper import MetalScraper
-from bs4 import BeautifulSoup
 
-USER_AGENTS = user_agent_list = [
-    #Chrome
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-    #Firefox
-    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
-    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
-    'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
-    'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
-    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
-    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
-    'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
-    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
-    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
-    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
-    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
-    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'
-]
-LOCAL = True
+HOST_NAME = 'localhost'
+PORT_NUMBER = 9000
 
+class MyHandler(BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        paths = {
+            '/api': {
+              'status': 200,
+              'content-type': 'text/json'
+            },
+            '/foo': {'status': 200},
+            '/bar': {'status': 302},
+            '/baz': {'status': 404},
+            '/qux': {'status': 500}
+        }
+
+        if self.path in paths:
+          self.respond(paths[self.path])
+        else:
+          self.respond({'status': 500})
+
+    def handle_http(self, status_code, path):
+      self.send_response(status_code)
+      self.send_header('Content-type', 'text/html')
+      self.end_headers()
+      content = ''
+      if(path == '/api'):
+        randomband = RandomBand()
+        band = randomband.get()
+        content = str(band)
+
+      """
+        content = '''
+        <html><head><title>Title goes here.</title></head>
+        <body><p>This is a test.</p>
+        <p>You accessed path: {}</p>
+        </body></html>
+        '''.format(path)
+        """
+      return bytes(content, 'UTF-8')
+
+    def respond(self, opts):
+        response = self.handle_http(opts['status'], self.path)
+        self.wfile.write(response)
+
+if __name__ == '__main__':
+    server_class = HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    print(time.asctime(), 'Server Starts - %s:%s' % (HOST_NAME, PORT_NUMBER))
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print(time.asctime(), 'Server Stops - %s:%s' % (HOST_NAME, PORT_NUMBER))
+
+"""
 def get_maximum_id():
     
     if(LOCAL):
@@ -41,13 +71,13 @@ def get_maximum_id():
         enc = 'utf-8'
         with open(home_url, 'r', encoding=enc) as f:
             data = f.read()
-    """ 
+    
     else:
         user_agent = random.choice(USER_AGENTS)
         request = urllib.request.Request(band_url)
         request.add_header('User-Agent', user_agent)
         data = urllib.request.urlopen(request).read()
-    """
+    
     soup = BeautifulSoup(data, 'html.parser')
     latest_url = soup.find('div', {'id': 'additionBands'}).find('a')['href']
     latest_url = latest_url.split('/')
@@ -71,48 +101,12 @@ def get_file(bandId = 0):
 
     return data
 
-def get_band_stats(soup):
-    dts = soup.find_all("dt")
-    stats_keys = []
-    for key in dts:
-        stats_keys.append(key.get_text().lower().replace(" ", "_").strip(":"))
-    dds = soup.find_all("dd")
-    stats_values = []
-    for value in dds:
-        stats_values.append(value.get_text().lower().replace(
-            "\n", " ").replace("\t", " ").strip())
-    band_stats = dict(zip(stats_keys, stats_values))
-    return band_stats
 
-def cache_band_logo(logo_url):
-    filename = logo_url.split('/')
-    filename = 'cache/' + filename.pop()
 
-    user_agent = random.choice(USER_AGENTS)
-    request = urllib.request.Request(logo_url)
-    request.add_header('User-Agent', user_agent)
-    data = urllib.request.urlopen(request).read()
 
-    f = open(filename, 'wb')
-    f.write(data)
-    f.close()
-    
-    return filename
 
-# From https://github.com/alikoneko/metal-scraper/
-def save(band):
-    placeholder = ", ".join(["?"] * len(band))
-    fields = ','.join(band.keys())
-    values = band.values()
-    #print(fields)
-    #print(values)
-    sql = "INSERT INTO `{table}` ({columns}) VALUES ({values})".format(table="maBands", columns=fields, values=placeholder)
-    #print(sql)
-    conn = sqlite3.connect('db/bands.sqlite3')
-    cur = conn.cursor()
-    cur.execute(sql, list(values))
-    conn.commit()
-    return cur.lastrowid
+
+
 
 def run():
 
@@ -122,24 +116,8 @@ def run():
 
     #get_maximum_id()
 
-    """
-    html = get_file()
-    soup = BeautifulSoup(html, 'html.parser')
-    band = {}
 
-    band_url = soup.find("input", {"name":"origin"}).get('value')
-    band_url = band_url.split('/')
-    band["ma_id"] = band_url.pop()
-    name_div = soup.find("h1", {"class":"band_name"})
-    band["name"] = name_div.find("a").get_text() if name_div else None
-    logo_div = soup.find("a", {"id":"logo"})
-    band["live_logo_url"] = logo_div["href"] if logo_div else None
-    band["cached_logo_url"] = cache_band_logo(band["live_logo_url"])
-    stats = get_band_stats(soup)
-    band.update(stats)
+    html = get_file()
+    
     #save(band)
     """
-
-if __name__ == "__main__":
-    run()
-    pass
--- libs/RandomBand.py
+++ libs/RandomBand.py
@@ -4,11 +4,117 @@ import urllib.request
 import sqlite3
 from bs4 import BeautifulSoup
 from datetime import datetime
+from libs.scraper.ScraperHelper import ScraperHelper
 
 DB = 'db/bands.sqlite3'
 CONN = sqlite3.connect(DB)
-LOCAL = True
 
+class RandomBand:
+  def __init__(self):
+    self.target = 'https://www.metal-archives.com/band/view/id/'
+
+  def get(self):
+    randomband = self.getrandomband()    
+    if(randomband['played'] == 1):
+      band = self.getband(randomband['id'])
+    else:
+      self.scrapeband(randomband['id'], randomband['ma_id'])
+      band = self.getband(randomband['id'])
+      
+    return band
+
+  def getband(self, id):
+    cur = CONN.cursor()
+    sql = "SELECT * FROM maBands WHERE id = " + str(id) + " LIMIT 1;"
+    cur.execute(sql)
+    row = cur.fetchone()
+    return row
+    
+  def getrandomband(self):
+    cur = CONN.cursor()
+    sql = "SELECT id, ma_id, played FROM maBands ORDER BY RANDOM() LIMIT 1"
+    cur.execute(sql)
+    row = cur.fetchone()
+    #print(row)
+    return {
+      'id': row[0],
+      'ma_id': row[1],
+      'played': row[2]
+    }
+  
+  def scrapeband(self, id, maid):
+    maurl = self.target + str(maid)
+    #maurl = 'http://localhost/ma-test/1.html'
+    scraperhelper = ScraperHelper()
+    html = scraperhelper.get(maurl)    
+    soup = BeautifulSoup(html, 'html.parser')
+    band = {}
+    name_div = soup.find("h1", {"class":"band_name"})
+    band["name"] = name_div.find("a").get_text() if name_div else None
+    logo_div = soup.find("a", {"id":"logo"})
+    band["live_logo_url"] = logo_div["href"] if logo_div else None
+    band["cached_logo_url"] = self.cache_band_logo(band["live_logo_url"])
+    band["played"] = 1
+    stats = self.get_band_stats(soup)
+    band.update(stats)
+    self.save(id, band)
+
+  def cache_band_logo(self, logo_url):
+    if not logo_url:
+      return ''
+    logo_url = logo_url.split('?')[0]
+    filename = logo_url.split('/')
+    filename = 'cache/' + filename.pop()
+    scraperhelper = ScraperHelper()
+    data = scraperhelper.get(logo_url)
+    f = open(filename, 'wb')
+    f.write(data)
+    f.close()
+    return filename
+
+  # From https://github.com/alikoneko/metal-scraper/
+  def get_band_stats(self, soup):
+      dts = soup.find_all("dt")
+      stats_keys = []
+      for key in dts:
+          stats_keys.append(key.get_text().lower().replace(" ", "_").strip(":"))
+      dds = soup.find_all("dd")
+      stats_values = []
+      for value in dds:
+          stats_values.append(value.get_text().lower().replace(
+              "\n", " ").replace("\t", " ").strip())
+      band_stats = dict(zip(stats_keys, stats_values))
+      return band_stats
+
+  def save(self, id, band):
+    items = band.items()
+    update = []
+    for item in items:
+      update.append(str(item[0]) + "=\"" + str(item[1]) + "\"")
+
+    sql = "UPDATE maBands SET " + ', '.join(update) + " WHERE id = " + str(id) + ";"
+
+    cur = CONN.cursor()
+    cur.execute(sql)
+    CONN.commit()
+    
+    """
+    placeholder = ", ".join(["?"] * len(band))
+    fields = ','.join(band.keys())
+    values = band.values()
+    #print(fields)
+    #print(values)
+    sql = "UPDATE maBands "
+    sql = "INSERT INTO `{table}` ({columns}) VALUES ({values})".format(table="maBands", columns=fields, values=placeholder)
+    #print(sql)
+    conn = sqlite3.connect('db/bands.sqlite3')
+    cur = conn.cursor()
+    cur.execute(sql, list(values))
+    conn.commit()
+    return cur.lastrowid
+    """
+
+"""
 class RandomBand:
     def get(self):
         lastupdateddate = float(self.getlastupdateddate())
@@ -111,3 +217,4 @@ class RandomBand:
         cur.execute(sql, values)
         CONN.commit()
         return currenttimestamp
+"""
